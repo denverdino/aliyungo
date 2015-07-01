591a04bcc1fcf3389d3528f864b545c662899191
yufeizyf <yufei.zyf@alibaba-inc.com>
2015-07-01 14:03:07 +0800

    Update instances_test.go
    add examples

--- ecs/instances_test.go
+++ ecs/instances_test.go
@@ -3,6 +3,91 @@ package ecs
 import (
 	"testing"
 )
+func ExampleClient_DescribeInstanceStatus(t *testing.T) {
+	t.Logf("DescribeInstanceStatus Example\n")
+
+	pagination := &Pagination{1, 1}
+	regionId := Region("cn-beijing")
+	zoneId := "cn-beijing-b"
+
+	var describeInstanceStatusArgs DescribeInstanceStatusArgs
+	describeInstanceStatusArgs.RegionId = regionId
+	describeInstanceStatusArgs.ZoneId = zoneId
+	describeInstanceStatusArgs.Pagination = *pagination
+
+	client := NewClient(TestAccessKeyId, TestAccessKeySecret)
+	instanceStatus, _, err := client.DescribeInstanceStatus(&describeInstanceStatusArgs)
+
+	if err != nil {
+		t.Errorf("Failed to describe Instance: %s status:%v", TestInstanceId, err)
+	} else {
+		for i := 0; i < len(instanceStatus); i++ {
+			t.Logf("Instance %s Status: %s ", instanceStatus[i].InstanceId, instanceStatus[i].Status)
+		}
+	}
+}
+
+func ExampleClient_DescribeInstanceAttribute(t *testing.T) {
+	t.Logf("DescribeInstanceAttribute Example\n")
+
+	client := NewClient(TestAccessKeyId, TestAccessKeySecret)
+
+	instanceAttributeType, err := client.DescribeInstanceAttribute(TestInstanceId)
+
+	if err != nil {
+		t.Errorf("Failed to describe Instance %s attribute: %v", TestInstanceId, err)
+	} else {
+		t.Logf("Instance Information")
+		t.Logf("InstanceId = %s ", instanceAttributeType.InstanceId)
+		t.Logf("InstanceName = %s ", instanceAttributeType.InstanceName)
+		t.Logf("HostName = %s ", instanceAttributeType.HostName)
+		t.Logf("ZoneId = %s ", instanceAttributeType.ZoneId)
+		t.Logf("RegionId = %s ", instanceAttributeType.RegionId)
+	}
+}
+
+func ExampleClient_DescribeInstanceVncUrl(t *testing.T) {
+	t.Logf("DescribeInstanceVncUrl Example\n")
+
+	region := Region("cn-beijing")
+
+	var describeInstanceVncUrlArgs DescribeInstanceVncUrlArgs
+	describeInstanceVncUrlArgs.RegionId = region
+	describeInstanceVncUrlArgs.InstanceId = TestInstanceId
+
+	client := NewClient(TestAccessKeyId, TestAccessKeySecret)
+
+	instanceVncUrl, err := client.DescribeInstanceVncUrl(&describeInstanceVncUrlArgs)
+
+	if err != nil {
+		t.Errorf("Failed to describe Instance %s vnc url: %v", TestInstanceId, err)
+	} else {
+		t.Logf("VNC URL = %s ", instanceVncUrl)
+	}
+}
+
+func ExampleClient_StopInstance(t *testing.T) {
+	t.Logf("Stop Instance Example\n")
+
+	client := NewClient(TestAccessKeyId, TestAccessKeySecret)
+
+	err := client.StopInstance(TestInstanceId, true)
+
+	if err != nil {
+		t.Errorf("Failed to stop Instance %s vnc url: %v", TestInstanceId, err)
+	}
+}
+
+func ExampleClient_DeleteInstance(t *testing.T) {
+	t.Logf("Delete Instance Example")
+	client := NewClient(TestAccessKeyId, TestAccessKeySecret)
+
+	err := client.DeleteInstance(TestInstanceId)
+
+	if err != nil {
+		t.Errorf("Failed to delete Instance %s vnc url: %v", TestInstanceId, err)
+	}
+}
 
 func TestECSInstance(t *testing.T) {
 

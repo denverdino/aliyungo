83c07d3faa01768598b87f740e4226beee98815d
姜继忠 <jizhong.jiangjz@aliaba-inc.com>
2016-07-11 18:53:41 +0800

    支持从环境变量获取endpoint
    endpoint优先级：code < env < default
    
    Signed-off-by: 姜继忠 <jizhong.jiangjz@aliaba-inc.com>

--- dns/client.go
+++ dns/client.go
@@ -2,6 +2,7 @@ package dns
 
 import (
 	"github.com/denverdino/aliyungo/common"
+	"os"
 )
 
 type Client struct {
@@ -16,13 +17,21 @@ const (
 
 // NewClient creates a new instance of DNS client
 func NewClient(accessKeyId, accessKeySecret string) *Client {
-	client := &Client{}
-	client.Init(DNSDefaultEndpoint, DNSAPIVersion, accessKeyId, accessKeySecret)
-	return client
+	endpoint := os.Getenv("DNS_ENDPOINT")
+	if endpoint == "" {
+		endpoint = DNSDefaultEndpoint
+	}
+	return NewClientWithEndpoint(endpoint, accessKeyId, accessKeySecret)
 }
 
 // NewCustomClient creates a new instance of ECS client with customized API endpoint
 func NewCustomClient(accessKeyId, accessKeySecret string, endpoint string) *Client {
+	client := &Client{}
+	client.Init(endpoint, DNSAPIVersion, accessKeyId, accessKeySecret)
+	return client
+}
+
+func NewClientWithEndpoint(endpoint string, accessKeyId, accessKeySecret string) *Client {
 	client := &Client{}
 	client.Init(endpoint, DNSAPIVersion, accessKeyId, accessKeySecret)
 	return client

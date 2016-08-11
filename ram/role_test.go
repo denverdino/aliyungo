e20c256520be68948a199b3a1492464f27218bff
YoungChen <chainone@gmail.com>
2016-08-11 15:09:53 +0800

    Revert "Moved role test to a separate package: role_test"
    This reverts commit 4dc107ca558905853e1f5771ac5d093067e4e4e1.

--- ram/role_test.go
+++ ram/role_test.go
@@ -1,4 +1,4 @@
-package ram_test
+package ram
 
 import (
 	"encoding/json"
@@ -6,8 +6,6 @@ import (
 	"strconv"
 	"testing"
 	"time"
-
-	"github.com/denverdino/aliyungo/ram"
 )
 
 /*
@@ -18,32 +16,32 @@ var (
 	accountId = os.Getenv("AccountId")
 	roleName  = strconv.FormatInt(time.Now().Unix(), 10)
 
-	princpal = ram.AssumeRolePolicyPrincpal{RAM: []string{"acs:ram::" + accountId + ":root"}}
+	princpal = AssumeRolePolicyPrincpal{RAM: []string{"acs:ram::" + accountId + ":root"}}
 
-	policyDocument = ram.AssumeRolePolicyDocument{
-		Statement: []ram.AssumeRolePolicyItem{
-			ram.AssumeRolePolicyItem{Action: "sts:AssumeRole", Effect: "Allow", Principal: princpal},
+	policyDocument = AssumeRolePolicyDocument{
+		Statement: []AssumeRolePolicyItem{
+			AssumeRolePolicyItem{Action: "sts:AssumeRole", Effect: "Allow", Principal: princpal},
 		},
 		Version: "1"}
 
-	newPolicyDocument = ram.AssumeRolePolicyDocument{
-		Statement: []ram.AssumeRolePolicyItem{
-			ram.AssumeRolePolicyItem{Action: "sts:AssumeRole", Effect: "Deny", Principal: princpal},
+	newPolicyDocument = AssumeRolePolicyDocument{
+		Statement: []AssumeRolePolicyItem{
+			AssumeRolePolicyItem{Action: "sts:AssumeRole", Effect: "Deny", Principal: princpal},
 		},
 		Version: "1"}
 
-	RoleReq = ram.RoleRequest{
+	role = RoleRequest{
 		RoleName:                 roleName,
 		AssumeRolePolicyDocument: getAssumeRolePolicyDocumentStr(),
 		Description:              "this is a role for unit test purpose",
 	}
 
-	updateRoleRequest = ram.UpdateRoleRequest{
+	updateRoleRequest = UpdateRoleRequest{
 		RoleName:                    roleName,
 		NewAssumeRolePolicyDocument: getNewAssumeRolePolicyDocumentStr(),
 	}
 
-	roleQuery = ram.RoleQueryRequest{RoleName: roleName}
+	roleQuery = RoleQueryRequest{RoleName: roleName}
 )
 
 func getAssumeRolePolicyDocumentStr() string {
@@ -57,8 +55,8 @@ func getNewAssumeRolePolicyDocumentStr() string {
 }
 
 func TestCreateRole(t *testing.T) {
-	client := ram.NewTestClient()
-	resp, err := client.CreateRole(RoleReq)
+	client := NewTestClient()
+	resp, err := client.CreateRole(role)
 	if err != nil {
 		t.Errorf("Failed to CreateRole %v", err)
 	}
@@ -66,7 +64,7 @@ func TestCreateRole(t *testing.T) {
 }
 
 func TestGetRole(t *testing.T) {
-	client := ram.NewTestClient()
+	client := NewTestClient()
 	resp, err := client.GetRole(roleQuery)
 	if err != nil {
 		t.Errorf("Failed to GetRole %v", err)
@@ -75,7 +73,7 @@ func TestGetRole(t *testing.T) {
 }
 
 func TestUpdateRole(t *testing.T) {
-	client := ram.NewTestClient()
+	client := NewTestClient()
 	resp, err := client.UpdateRole(updateRoleRequest)
 	if err != nil {
 		t.Errorf("Failed to UpdateRole %v", err)
@@ -84,7 +82,7 @@ func TestUpdateRole(t *testing.T) {
 }
 
 func TestListRoles(t *testing.T) {
-	client := ram.NewTestClient()
+	client := NewTestClient()
 	resp, err := client.ListRoles()
 	if err != nil {
 		t.Errorf("Failed to ListRoles %v", err)
@@ -93,8 +91,8 @@ func TestListRoles(t *testing.T) {
 }
 
 func TestDeleteRole(t *testing.T) {
-	client := ram.NewTestClient()
-	resp, err := client.DeleteRole(ram.RoleQueryRequest{RoleName: roleName})
+	client := NewTestClient()
+	resp, err := client.DeleteRole(RoleQueryRequest{RoleName: roleName})
 	if err != nil {
 		t.Errorf("Failed to DeleteRole %v", err)
 	}

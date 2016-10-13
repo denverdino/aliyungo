d550d70d63d4214dcb8081b1e4edcc21ce001179
治世 <xiaobing.meng@alibaba-inc.com>
2016-10-13 15:39:38 +0800

    fix

--- common/types.go
+++ common/types.go
@@ -1,8 +1,12 @@
 package common
 
 type InternetChargeType string
+type InstanceChargeType string
 
 const (
 	PayByBandwidth = InternetChargeType("PayByBandwidth")
 	PayByTraffic   = InternetChargeType("PayByTraffic")
+
+	PrePaid  = InstanceChargeType("PrePaid")  //包年包月
+	PostPaid = InstanceChargeType("PostPaid") //按量付费
 )

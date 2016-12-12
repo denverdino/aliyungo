44ba095487bb37b1cc1e6ee2cb5a0ed3ca5c7ed3
治世 <xiaobing.meng@alibaba-inc.com>
2016-12-12 11:22:16 +0800

    add new region bj-hpc

--- common/regions.go
+++ common/regions.go
@@ -8,6 +8,7 @@ const (
 	Hangzhou        = Region("cn-hangzhou")
 	Qingdao         = Region("cn-qingdao")
 	Beijing         = Region("cn-beijing")
+	BeiJingHpc      = Region("cn-beijing-hpc")
 	Hongkong        = Region("cn-hongkong")
 	Shenzhen        = Region("cn-shenzhen")
 	USWest1         = Region("us-west-1")
@@ -16,4 +17,4 @@ const (
 	ShenZhenFinance = Region("cn-shenzhen-finance-1")
 )
 
-var ValidRegions = []Region{Hangzhou, Qingdao, Beijing, Shenzhen, Hongkong, Shanghai, USWest1, APSouthEast1, ShenZhenFinance}
+var ValidRegions = []Region{Hangzhou, Qingdao, Beijing, BeiJingHpc, Shenzhen, Hongkong, Shanghai, USWest1, APSouthEast1, ShenZhenFinance}

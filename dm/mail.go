33108037ba71d321a477d9be4e3ce45d68ada3f0
yrs224 <yrs224@qq.com>
2017-01-30 22:57:30 +0800

    升级了Invoke方法，增加了Path参数

--- dm/mail.go
+++ dm/mail.go
@@ -26,7 +26,7 @@ type SendBatchMailArgs struct {
 //please set the receiverName and template in the console of Aliyun before you call this API,if you use tagName, you should set it as well
 
 func (this *Client) SendBatchMail(args *SendBatchMailArgs) error {
-	return this.InvokeByAnyMethod(http.MethodPost, BatchSendMail, args, &common.Response{})
+	return this.InvokeByAnyMethod(http.MethodPost, BatchSendMail, "", args, &common.Response{})
 }
 
 type SendSingleMailArgs struct {
@@ -48,6 +48,6 @@ type SendSingleMailArgs struct {
 //fromAlias, subject, htmlBody, textBody are optional
 
 func (this *Client) SendSingleMail(args *SendSingleMailArgs) error {
-	return this.InvokeByAnyMethod(http.MethodPost, SingleSendMail, args, &common.Response{})
+	return this.InvokeByAnyMethod(http.MethodPost, SingleSendMail, "", args, &common.Response{})
 }
 
--- sms/sms.go
+++ sms/sms.go
@@ -15,5 +15,5 @@ type SendSmsArgs struct {
 
 //please set the signature and template in the console of Aliyun before you call this API
 func (this *Client) SendSms(args *SendSmsArgs) error {
-	return this.InvokeByAnyMethod(http.MethodPost, SingleSendSms, args, &common.Response{})
+	return this.InvokeByAnyMethod(http.MethodPost, SingleSendSms, "", args, &common.Response{})
 }

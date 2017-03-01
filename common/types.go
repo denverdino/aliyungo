26df932e2853d6984c6f16f1bf5d6987193821f0
治世 <xiaobing.meng@alibaba-inc.com>
2017-03-01 20:21:14 +0800

    支持从LocationService获取endpoint

--- common/types.go
+++ common/types.go
@@ -13,3 +13,27 @@ const (
 	PrePaid  = InstanceChargeType("PrePaid")
 	PostPaid = InstanceChargeType("PostPaid")
 )
+
+type ServiceCode string
+
+type DescribeEndpointArgs struct {
+	Id          Region
+	ServiceCode string
+	Type        string
+}
+
+type EndpointItem struct {
+	Protocols struct {
+		Protocols []string
+	}
+	Type        string
+	Namespace   string
+	Id          Region
+	SerivceCode string
+	Endpoint    string
+}
+
+type DescribeEndpointResponse struct {
+	Response
+	EndpointItem
+}

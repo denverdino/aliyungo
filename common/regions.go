70dbe6cdc6af04d7f310bc01c7674ba38a78c65b
治世 <xiaobing.meng@alibaba-inc.com>
2017-04-13 14:12:25 +0800

    update regions

--- common/regions.go
+++ common/regions.go
@@ -11,7 +11,9 @@ const (
 	BeiJingHpc      = Region("cn-beijing-hpc")
 	Hongkong        = Region("cn-hongkong")
 	Shenzhen        = Region("cn-shenzhen")
+	ZhangJiaKou     = Region("cn-zhangjiakou")
 	USWest1         = Region("us-west-1")
+	USEast1         = Region("us-east-1")
 	APSouthEast1    = Region("ap-southeast-1")
 	APNorthEast1    = Region("ap-northeast-1")
 	Shanghai        = Region("cn-shanghai")
@@ -21,6 +23,6 @@ const (
 	APSouthEast2    = Region("ap-southeast-2") //澳洲
 )
 
-var ValidRegions = []Region{Hangzhou, Qingdao, Beijing, BeiJingHpc, Shenzhen, Hongkong,
-	Shanghai, USWest1, APSouthEast1, APNorthEast1, ShenZhenFinance,
+var ValidRegions = []Region{Hangzhou, Qingdao, Beijing, BeiJingHpc, Shenzhen, ZhangJiaKou, Hongkong,
+	Shanghai, USWest1, USEast1, APSouthEast1, APNorthEast1, ShenZhenFinance,
 	EUCentral1, MEEast1, APSouthEast2}

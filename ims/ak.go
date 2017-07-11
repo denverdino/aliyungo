89a47bcba879f89c748d8ef321a4c9624f7d1562
治世 <xiaobing.meng@alibaba-inc.com>
2017-07-11 14:14:31 +0800

    add ak api

---
+++ ims/ak.go
@@ -0,0 +1,74 @@
+package ims
+
+import "github.com/denverdino/aliyungo/common"
+
+type CreateAccessKeyRequest struct {
+	UserPrincipalName string
+}
+
+type CreateAccessKeyResponse struct {
+	common.Response
+	AccessKey AccessKey
+}
+
+type AccessKey struct {
+	AccessKeyId     string
+	AccessKeySecret string
+	CreateDate      string
+	Status          string
+}
+
+func (client *ImsClient) CreateAccessKey(args *CreateAccessKeyRequest) (*CreateAccessKeyResponse, error) {
+	response := &CreateAccessKeyResponse{}
+
+	err := client.Invoke("CreateAccessKey", args, response)
+	if err != nil {
+		return nil, err
+	}
+
+	return response, nil
+}
+
+type DeleteAccessKeyRequest struct {
+	UserAccessKeyId   string
+	UserPrincipalName string
+}
+
+type DeleteAccessKeyResponse struct {
+	common.Response
+}
+
+func (client *ImsClient) DeleteAccessKey(args *DeleteAccessKeyRequest) (*DeleteAccessKeyResponse, error) {
+
+	response := &DeleteAccessKeyResponse{}
+
+	err := client.Invoke("DeleteAccessKey", args, response)
+	if err != nil {
+		return nil, err
+	}
+
+	return response, nil
+}
+
+type ListAccessKeysRequest struct {
+	UserPrincipalName string
+}
+
+type ListAccessKeysResponse struct {
+	common.Response
+
+	AccessKeys struct {
+		AccessKey []AccessKey
+	}
+}
+
+func (client *ImsClient) ListAccessKeys(args *ListAccessKeysRequest) (*ListAccessKeysResponse, error) {
+	response := &ListAccessKeysResponse{}
+
+	err := client.Invoke("ListAccessKeys", args, response)
+	if err != nil {
+		return nil, err
+	}
+
+	return response, nil
+}
---
+++ ims/ak_test.go
@@ -0,0 +1,55 @@
+package ims
+
+import "testing"
+
+func TestImsClient_CreateAccessKey(t *testing.T) {
+	args := &CreateAccessKeyRequest{
+		UserPrincipalName: "user-1499677896@1312623533146984.onaliyun.com",
+	}
+
+	response, err := client.CreateAccessKey(args)
+	if err != nil {
+		t.Fatalf("Failed to CreateAccessKey %++v", err)
+	} else {
+		t.Logf("Response = %++v", response)
+	}
+}
+
+func TestImsClient_DeleteAccessKey(t *testing.T) {
+	createArgs := &CreateAccessKeyRequest{
+		UserPrincipalName: "user-1499677896@1312623533146984.onaliyun.com",
+	}
+
+	response, err := client.CreateAccessKey(createArgs)
+	if err != nil {
+		t.Fatalf("Failed to CreateAccessKey %++v", err)
+	} else {
+		args := &DeleteAccessKeyRequest{
+			UserAccessKeyId:   response.AccessKey.AccessKeyId,
+			UserPrincipalName: createArgs.UserPrincipalName,
+		}
+
+		resp, err := client.DeleteAccessKey(args)
+		if err != nil {
+			t.Fatalf("Failed to DeleteAccessKey %++v", err)
+		} else {
+			t.Logf("Response = %++v", resp)
+		}
+	}
+
+}
+
+func TestImsClient_ListAccessKeys(t *testing.T) {
+	args := &ListAccessKeysRequest{
+		UserPrincipalName: "user-1499677896@1312623533146984.onaliyun.com",
+	}
+
+	response, err := client.ListAccessKeys(args)
+	if err != nil {
+		t.Fatalf("Failed to ListAccessKeys %++v", err)
+	} else {
+		for index, ak := range response.AccessKeys.AccessKey {
+			t.Logf("ak[%d] = %++v", index, ak)
+		}
+	}
+}

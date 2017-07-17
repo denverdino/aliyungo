e1d588479e9d5c1492d467cbb0673c9cefb44c3d
治世 <xiaobing.meng@alibaba-inc.com>
2017-07-17 10:03:18 +0800

    revert

--- ims/base.go
+++ ims/base.go
@@ -1,7 +1,6 @@
 package ims
 
 type BaseRequest struct {
-	ProxyCallerIp                string
-	ProxyTrustTransportInfo      bool
-	ProxyCallerSecurityTransport bool
+	SourceIp        string
+	SecureTransport string
 }

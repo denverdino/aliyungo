1a58f95bb06e4a9dfd0e49472e81de4d58f31317
yrs224 <yrs224@qq.com>
2017-08-29 14:36:41 +0800

    Added DYSms API & Updated PUSH API

--- sms/sms.go
+++ sms/sms.go
@@ -6,14 +6,36 @@ import (
 	"github.com/denverdino/aliyungo/common"
 )
 
+//阿里云通信
 type SendSmsArgs struct {
+	PhoneNumbers    string
+	SignName        string
+	TemplateCode    string
+	TemplateParam   string
+	SmsUpExtendCode string `ArgName:"smsUpExtendCode"`
+	OutId           string
+}
+
+type SendSmsResponse struct {
+	common.Response
+	Code string
+	Message string
+	BizId string
+}
+
+func (this *DYSmsClient) SendSms(args *SendSmsArgs) (*SendSmsResponse, error) {
+	resp := SendSmsResponse{}
+	return &resp, this.InvokeByAnyMethod(http.MethodGet, SendSms, "", args, &resp)
+}
+
+//邮件推送产品短信功能
+type SingleSendSmsArgs struct {
 	SignName string
 	TemplateCode  string
 	RecNum        string
 	ParamString   string
 }
 
-//please set the signature and template in the console of Aliyun before you call this API
-func (this *Client) SendSms(args *SendSmsArgs) error {
+func (this *Client) SingleSendSms(args *SingleSendSmsArgs) error {
 	return this.InvokeByAnyMethod(http.MethodPost, SingleSendSms, "", args, &common.Response{})
 }

ad8b1e6c426b261b764b7a46849a0ac38e3e7fd5
Charles <33474450+jancco2012@users.noreply.github.com>
2017-11-27 23:27:11 +0800

    Support to create Clients from credentials of assumed role (#205)
    * Support to create Clients from credentials of assumed role
    
    * Set RegionId as request parameter

--- sts/client.go
+++ sts/client.go
@@ -17,15 +17,29 @@ type STSClient struct {
 }
 
 func NewClient(accessKeyId string, accessKeySecret string) *STSClient {
+	return NewClientWithSecurityToken(accessKeyId, accessKeySecret, "")
+}
+
+func NewClientWithSecurityToken(accessKeyId string, accessKeySecret string, securityToken string) *STSClient {
 	endpoint := os.Getenv("STS_ENDPOINT")
 	if endpoint == "" {
 		endpoint = STSDefaultEndpoint
 	}
-	return NewClientWithEndpoint(endpoint, accessKeyId, accessKeySecret)
+
+	return NewClientWithEndpointAndSecurityToken(endpoint, accessKeyId, accessKeySecret, securityToken)
 }
 
 func NewClientWithEndpoint(endpoint string, accessKeyId string, accessKeySecret string) *STSClient {
+	return NewClientWithEndpointAndSecurityToken(endpoint, accessKeyId, accessKeySecret, "")
+}
+
+func NewClientWithEndpointAndSecurityToken(endpoint string, accessKeyId string, accessKeySecret string, securityToken string) *STSClient {
 	client := &STSClient{}
-	client.Init(endpoint, STSAPIVersion, accessKeyId, accessKeySecret)
+	client.WithEndpoint(endpoint).
+		WithVersion(STSAPIVersion).
+		WithAccessKeyId(accessKeyId).
+		WithAccessKeySecret(accessKeySecret).
+		WithSecurityToken(securityToken).
+		InitClient()
 	return client
 }

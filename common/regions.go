33c05d6cfafb561457f3645ffceb03ebc7132e22
He Guimin <guimin.hgm@alibaba-inc.com>
2017-11-29 21:01:01 +0800

    add wait routerinterface; modify ess group int type; add new region

--- common/regions.go
+++ common/regions.go
@@ -16,6 +16,7 @@ const (
 	APSouthEast1 = Region("ap-southeast-1")
 	APNorthEast1 = Region("ap-northeast-1")
 	APSouthEast2 = Region("ap-southeast-2")
+	APSouthEast3 = Region("ap-southeast-3")
 
 	USWest1 = Region("us-west-1")
 	USEast1 = Region("us-east-1")
@@ -23,12 +24,13 @@ const (
 	MEEast1 = Region("me-east-1")
 
 	EUCentral1 = Region("eu-central-1")
+
 )
 
 var ValidRegions = []Region{
 	Hangzhou, Qingdao, Beijing, Shenzhen, Hongkong, Shanghai, Zhangjiakou,
 	USWest1, USEast1,
-	APNorthEast1, APSouthEast1, APSouthEast2,
+	APNorthEast1, APSouthEast1, APSouthEast2,APSouthEast3,
 	MEEast1,
 	EUCentral1,
 }

86799dd245f85ab39a4db06fe1eccecb967c17c7
xiaobing.meng <xiaobing.meng@alibaba-inc.com>
2018-01-08 10:24:03 +0800

    fix location api

--- common/endpoint_test.go
+++ common/endpoint_test.go
@@ -7,3 +7,9 @@ import (
 func TestLoadEndpointFromFile(t *testing.T) {
 
 }
+
+func TestClient_DescribeOpenAPIEndpoint(t *testing.T) {
+	client := NewTestClientForDebug()
+	endpoint := client.DescribeOpenAPIEndpoint(Region(TestRegionId), TestServiceCode)
+	t.Logf("endpoint=%s", endpoint)
+}

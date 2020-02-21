17954a7f991bf9cd53380efed8bb8aa7e15b5a60
fredkan <kjb0518@163.com>
2020-02-21 11:59:07 +0800

    support ecs tag

--- ecs/disks.go
+++ ecs/disks.go
@@ -139,7 +139,7 @@ type CreateDiskArgs struct {
 	Encrypted    bool
 	DiskCategory DiskCategory
 	Size         int
-        Tag          map[string]string
+	Tag          map[string]string
 	SnapshotId   string
 	ClientToken  string
 	KMSKeyID     string

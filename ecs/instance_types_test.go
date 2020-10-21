b53e15dcf464ef181bec99073ae21f9669f1a6ab
bingshen.wbs <bingshen.wbs@alibaba-inc.com>
2020-10-21 14:47:01 +0800

    add new DescribeInstanceTypes args "InstanceTypes"
    Signed-off-by: bingshen.wbs <bingshen.wbs@alibaba-inc.com>

--- ecs/instance_types_test.go
+++ ecs/instance_types_test.go
@@ -5,11 +5,20 @@ import (
 )
 
 func TestDescribeInstanceTypes(t *testing.T) {
-
 	client := NewTestClient()
 	instanceTypes, err := client.DescribeInstanceTypes()
 	if err != nil {
 		t.Fatalf("Failed to DescribeInstanceTypes: %v", err)
+	}
+	for _, instanceType := range instanceTypes {
+		t.Logf("InstanceType: %++v", instanceType)
+	}
+
+	instanceTypes, err = client.DescribeInstanceTypesNew(&DescribeInstanceTypesArgs{
+		InstanceTypes: []string{"ecs.ec5.24xlarge", "ecs.ddh6s.custom.c4m48"},
+	})
+	if err != nil {
+		t.Fatalf("Failed to DescribeInstanceTypesNew: %v", err)
 	}
 	for _, instanceType := range instanceTypes {
 		t.Logf("InstanceType: %++v", instanceType)

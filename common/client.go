f281dbed62591678623538332267a8421420c816
yuejiarui.yjr <yuejiarui.yjr@alibaba-inc.com>
2022-05-16 19:26:12 +0800

    sdk support logging

--- common/client.go
+++ common/client.go
@@ -43,6 +43,7 @@ type Client struct {
 	userAgent       string
 	disableTrace    bool
 	span            opentracing.Span
+	logger          *Logger
 }
 
 // Initialize properties of a client instance
@@ -302,6 +303,12 @@ func (client *Client) WithDisableTrace(disableTrace bool) *Client {
 	return client
 }
 
+// WithUserAgent sets user agent to the request/response message
+func (client *Client) WithSpan(span opentracing.Span) *Client {
+	client.SetSpan(span)
+	return client
+}
+
 // ----------------------------------------------------
 // SetXXX methods
 // ----------------------------------------------------
@@ -404,15 +411,17 @@ func (client *Client) initEndpoint() error {
 }
 
 // Invoke sends the raw HTTP request for ECS services
-func (client *Client) Invoke(action string, args interface{}, response interface{}) error {
+func (client *Client) Invoke(action string, args interface{}, response interface{}) (err error) {
 	if err := client.ensureProperties(); err != nil {
 		return err
 	}
 
-	//init endpoint
-	//if err := client.initEndpoint(); err != nil {
-	//	return err
-	//}
+	// log request
+	fieldMap := make(map[string]string)
+	initLogMsg(fieldMap)
+	defer func() {
+		client.printLog(fieldMap, err)
+	}()
 
 	request := Request{}
 	request.init(client.version, action, client.AccessKeyId, client.securityToken, client.regionID)
@@ -434,7 +443,6 @@ func (client *Client) Invoke(action string, args interface{}, response interface
 
 	// TODO move to util and add build val flag
 	httpReq.Header.Set("X-SDK-Client", `AliyunGO/`+Version+client.businessInfo)
-
 	httpReq.Header.Set("User-Agent", httpReq.UserAgent()+" "+client.userAgent)
 
 	// Set tracer
@@ -460,15 +468,20 @@ func (client *Client) Invoke(action string, args interface{}, response interface
 			opentracing.HTTPHeadersCarrier(httpReq.Header))
 	}
 
+	putMsgToMap(fieldMap, httpReq)
 	t0 := time.Now()
+	fieldMap["{start_time}"] = t0.Format("2006-01-02 15:04:05")
 	httpResp, err := client.httpClient.Do(httpReq)
 	t1 := time.Now()
+	fieldMap["{cost}"] = t1.Sub(t0).String()
 	if err != nil {
 		if span != nil {
 			ext.LogError(span, err)
 		}
 		return GetClientError(err)
 	}
+	fieldMap["{code}"] = strconv.Itoa(httpResp.StatusCode)
+	fieldMap["{res_headers}"] = TransToString(httpResp.Header)
 	statusCode := httpResp.StatusCode
 
 	if client.debug {
@@ -481,6 +494,7 @@ func (client *Client) Invoke(action string, args interface{}, response interface
 
 	defer httpResp.Body.Close()
 	body, err := ioutil.ReadAll(httpResp.Body)
+	fieldMap["{res_body}"] = string(body)
 
 	if err != nil {
 		return GetClientError(err)
@@ -519,10 +533,17 @@ func (client *Client) Invoke(action string, args interface{}, response interface
 }
 
 // Invoke sends the raw HTTP request for ECS services
-func (client *Client) InvokeByFlattenMethod(action string, args interface{}, response interface{}) error {
+func (client *Client) InvokeByFlattenMethod(action string, args interface{}, response interface{}) (err error) {
 	if err := client.ensureProperties(); err != nil {
 		return err
 	}
+
+	// log request
+	fieldMap := make(map[string]string)
+	initLogMsg(fieldMap)
+	defer func() {
+		client.printLog(fieldMap, err)
+	}()
 
 	//init endpoint
 	if err := client.initEndpoint(); err != nil {
@@ -550,23 +571,58 @@ func (client *Client) InvokeByFlattenMethod(action string, args interface{}, res
 
 	// TODO move to util and add build val flag
 	httpReq.Header.Set("X-SDK-Client", `AliyunGO/`+Version+client.businessInfo)
-
 	httpReq.Header.Set("User-Agent", httpReq.UserAgent()+" "+client.userAgent)
 
+	// Set tracer
+	var span opentracing.Span
+	if ok := opentracing.IsGlobalTracerRegistered(); ok && !client.disableTrace {
+		tracer := opentracing.GlobalTracer()
+		var rootCtx opentracing.SpanContext
+
+		if client.span != nil {
+			rootCtx = client.span.Context()
+		}
+
+		span = tracer.StartSpan(
+			"AliyunGO-"+request.Action,
+			opentracing.ChildOf(rootCtx),
+			opentracing.Tag{string(ext.Component), "AliyunGO"},
+			opentracing.Tag{"ActionName", request.Action})
+
+		defer span.Finish()
+		tracer.Inject(
+			span.Context(),
+			opentracing.HTTPHeaders,
+			opentracing.HTTPHeadersCarrier(httpReq.Header))
+	}
+
+	putMsgToMap(fieldMap, httpReq)
 	t0 := time.Now()
+	fieldMap["{start_time}"] = t0.Format("2006-01-02 15:04:05")
 	httpResp, err := client.httpClient.Do(httpReq)
 	t1 := time.Now()
+	fieldMap["{cost}"] = t1.Sub(t0).String()
 	if err != nil {
+		if span != nil {
+			ext.LogError(span, err)
+		}
 		return GetClientError(err)
 	}
+	fieldMap["{code}"] = strconv.Itoa(httpResp.StatusCode)
+	fieldMap["{res_headers}"] = TransToString(httpResp.Header)
 	statusCode := httpResp.StatusCode
 
 	if client.debug {
 		log.Printf("Invoke %s %s %d (%v)", ECSRequestMethod, requestURL, statusCode, t1.Sub(t0))
 	}
 
+	if span != nil {
+		ext.HTTPStatusCode.Set(span, uint16(httpResp.StatusCode))
+	}
+
 	defer httpResp.Body.Close()
 	body, err := ioutil.ReadAll(httpResp.Body)
+	fieldMap["{res_body}"] = string(body)
 
 	if err != nil {
 		return GetClientError(err)
@@ -606,10 +662,17 @@ func (client *Client) InvokeByFlattenMethod(action string, args interface{}, res
 // Invoke sends the raw HTTP request for ECS services
 //改进了一下上面那个方法，可以使用各种Http方法
 //2017.1.30 增加了一个path参数，用来拓展访问的地址
-func (client *Client) InvokeByAnyMethod(method, action, path string, args interface{}, response interface{}) error {
+func (client *Client) InvokeByAnyMethod(method, action, path string, args interface{}, response interface{}) (err error) {
 	if err := client.ensureProperties(); err != nil {
 		return err
 	}
+
+	// log request
+	fieldMap := make(map[string]string)
+	initLogMsg(fieldMap)
+	defer func() {
+		client.printLog(fieldMap, err)
+	}()
 
 	//init endpoint
 	//if err := client.initEndpoint(); err != nil {
@@ -628,7 +691,6 @@ func (client *Client) InvokeByAnyMethod(method, action, path string, args interf
 	// Generate the request URL
 	var (
 		httpReq *http.Request
-		err     error
 	)
 	if method == http.MethodGet {
 		requestURL := client.endpoint + path + "?" + data.Encode()
@@ -648,20 +710,56 @@ func (client *Client) InvokeByAnyMethod(method, action, path string, args interf
 	httpReq.Header.Set("X-SDK-Client", `AliyunGO/`+Version+client.businessInfo)
 	httpReq.Header.Set("User-Agent", httpReq.Header.Get("User-Agent")+" "+client.userAgent)
 
+	// Set tracer
+	var span opentracing.Span
+	if ok := opentracing.IsGlobalTracerRegistered(); ok && !client.disableTrace {
+		tracer := opentracing.GlobalTracer()
+		var rootCtx opentracing.SpanContext
+
+		if client.span != nil {
+			rootCtx = client.span.Context()
+		}
+
+		span = tracer.StartSpan(
+			"AliyunGO-"+request.Action,
+			opentracing.ChildOf(rootCtx),
+			opentracing.Tag{string(ext.Component), "AliyunGO"},
+			opentracing.Tag{"ActionName", request.Action})
+
+		defer span.Finish()
+		tracer.Inject(
+			span.Context(),
+			opentracing.HTTPHeaders,
+			opentracing.HTTPHeadersCarrier(httpReq.Header))
+	}
+
+	putMsgToMap(fieldMap, httpReq)
 	t0 := time.Now()
+	fieldMap["{start_time}"] = t0.Format("2006-01-02 15:04:05")
 	httpResp, err := client.httpClient.Do(httpReq)
 	t1 := time.Now()
+	fieldMap["{cost}"] = t1.Sub(t0).String()
 	if err != nil {
+		if span != nil {
+			ext.LogError(span, err)
+		}
 		return GetClientError(err)
 	}
+	fieldMap["{code}"] = strconv.Itoa(httpResp.StatusCode)
+	fieldMap["{res_headers}"] = TransToString(httpResp.Header)
 	statusCode := httpResp.StatusCode
 
 	if client.debug {
 		log.Printf("Invoke %s %s %d (%v) %v", ECSRequestMethod, client.endpoint, statusCode, t1.Sub(t0), data.Encode())
 	}
 
+	if span != nil {
+		ext.HTTPStatusCode.Set(span, uint16(httpResp.StatusCode))
+	}
+
 	defer httpResp.Body.Close()
 	body, err := ioutil.ReadAll(httpResp.Body)
+	fieldMap["{res_body}"] = string(body)
 
 	if err != nil {
 		return GetClientError(err)
@@ -719,4 +817,16 @@ func GetCustomError(code, message string) error {
 		},
 		StatusCode: 400,
 	}
+}
+
+func putMsgToMap(fieldMap map[string]string, request *http.Request) {
+	fieldMap["{host}"] = request.Host
+	fieldMap["{method}"] = request.Method
+	fieldMap["{uri}"] = request.URL.RequestURI()
+	fieldMap["{pid}"] = strconv.Itoa(os.Getpid())
+	fieldMap["{version}"] = strings.Split(request.Proto, "/")[1]
+	hostname, _ := os.Hostname()
+	fieldMap["{hostname}"] = hostname
+	fieldMap["{req_headers}"] = TransToString(request.Header)
+	fieldMap["{target}"] = request.URL.Path + request.URL.RawQuery
 }
